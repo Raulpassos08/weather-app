593d84b2bccb9c90a949949641b466a6a6eac2d7
Raulpassos08 <raulfera2@gmail.com>
2024-10-30 09:50:36 -0300

    observable

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +8,9 @@ import { Injectable } from '@angular/core';
 export class WeatherService {
   private apiKey = 'de14dd081d5edaa5d8c7b092f5dd6bca';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getWeatherDatas(cityName: string): Observable<any>{
+    return this.http.get('', {})
+  }
 }
