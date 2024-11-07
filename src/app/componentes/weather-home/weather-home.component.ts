8f3cc54285a88e8e4c586d2202215e6468c71390
Raulpassos08 <raulpassos08@gmail.com>
2024-11-07 20:03:38 -0300

    services

--- src/app/componentes/weather-home/weather-home.component.ts
+++ src/app/componentes/weather-home/weather-home.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent {}
+export class WeatherHomeComponent implements OnInit {
+  ngOnInit(): void {}
+  constructor(private weatherService: WeatherService) {}
+}
